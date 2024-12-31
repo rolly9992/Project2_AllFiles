79a471b4b9a7e856d51cfcaa154f2631e417407a
Rob O'Mara <robertomara101@comcast.net>
2024-12-31 06:55:21 -0800

    process_data file updated
    updated the process_data.py file

--- data/process_data.py
+++ data/process_data.py
@@ -1,17 +1,61 @@
 
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
+
+
+
+file1 = r'C:\Users\Milo_Sand\Documents\GitHub\Project2\data\categories.csv' 
+#file1
+#sys.argv[2] =r'C:\Users\Milo_Sand\Documents\GitHub\Project2\data\messages.csv' 
+#sys.argv[3] = r'sqlite:///disaster_project.db'
+
+print(sys.argv)
+#sys.exit()
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    messages = pd.read_csv(messages_filepath,sep=',')
+    categories = pd.read_csv( categories_filepath,sep=',')
+    # merge datasets
+    df = messages.merge(categories,on='id',how='inner')
+  
+    return df
+    
 
 
 def clean_data(df):
-    pass
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';',expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+    category_colnames = [x[:-2] for x in row]
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+    
+    # drop the original categories column from `df`
+    df = df.drop('categories',axis=1)
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories],axis=1)
+    #drop duplicates 
+    df = df.drop_duplicates()
+    return df 
+    
 
 
 def save_data(df, database_filename):
-    pass  
+    engine = create_engine(database_filename) 
+    #'sqlite:///disaster_project.db')
+    df.to_sql('cleaned_messages', engine, index=False)
+    
+    #engine = create_engine(database_filename)
+    #df.to_sql('cleaned_messages', engine, index=False)
+    
+    #pass  
 
 
 def main():
